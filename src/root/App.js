6c00a2b35ad594febfc4fe4c5abe988325c9bee1
fatihkklnc <fatihkklnc@gmail.com>
2021-03-26 18:36:35 +0300

    ilk komit

---
+++ src/root/App.js
@@ -0,0 +1,22 @@
+
+
+
+import React from 'react';
+
+
+import Dashboard from './Dashboard';
+function App() {
+  
+  
+  return (
+    
+      <Dashboard>
+
+      </Dashboard>
+      
+    
+    
+  );
+}
+
+export default App;
