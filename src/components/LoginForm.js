0463aeafd4ee3df7e67a51645dc40741f0a28a42
fatihkklnc <fatihkklnc@gmail.com>
2021-03-30 16:53:00 +0300

    login ekranı aşamalı giriş

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,35 @@
-import React, { useState,useContext } from 'react'
+import React, { useState,useContext,useEffect } from 'react'
 import {HomeContext} from '../providers/HomeContext'
 function LoginForm() {
 
-    const {Login,error}=useContext(HomeContext);
+    const {Login,error,adminUser}=useContext(HomeContext);
 
-    const [details,setDetails]=useState({name:"",password:""})
+    const [details,setDetails]=useState({name:"",password:"",companyNo:""})
+    const [stateFirma,setStateFirma]=useState(false)
+    const [stateCompany,setStateCompany]=useState(false)
+    useEffect(() => {
+        if(adminUser.name == details.name && adminUser.password==details.password)
+        setStateFirma(true)
+        else
+        setStateFirma(false)
+        if(adminUser.name == details.name && adminUser.password==details.password && adminUser.companyNo==details.companyNo)
+        setStateCompany(true)
+        else
+        setStateCompany(false)
+    }, [details])
+    
     const handleChance=(event)=>{
+        
         let name=event.target.name;
         let value=event.target.value;
         setDetails({...details,[name]:value})
+        
     }
     const submitHandler=e=>{
         e.preventDefault();
         Login(details);
     }
+    
     return (
         <form onSubmit={submitHandler} className="form">
             <div className="form-inner">
@@ -28,6 +44,19 @@ function LoginForm() {
                     <label htmlFor="password">Password:</label>
                     <input type="password" name="password" id="password" onChange={handleChance}></input>
                 </div>
+                {(stateFirma)?//girilen kuladı şifreye göre firma no aktif in aktif durumu kontrolü
+                  <div className="form-group">
+                  <label htmlFor="companyNo">CompanyNo:</label>
+                  <input type="number" name="companyNo" id="companyNo" onChange={handleChance}></input>
+                 </div>:null
+                }
+                {(stateCompany)?
+                <div className="form-group">
+                    <label htmlFor="session">Session:</label>
+                    <input type="number" name="session" id="session" onChange={handleChance}></input>
+                </div>:null
+                }
+                
                 <input type="submit" value="Submit"></input>
             </div>
         </form>
--- src/providers/HomeContext.js
+++ src/providers/HomeContext.js
@@ -1,26 +1,30 @@
-import React,{createContext,useState} from 'react'
-
+import React,{createContext,useEffect,useState} from 'react'
+import axios from 'axios'
 export const HomeContext=createContext();
  export const HomeProvider =(props)=> {
-    const adminUser = {
-        name: "fatih",
-        password: "admin123",
-      };
+  const adminUser = {
+    name: "fatih",
+    password: "123",
+    companyNo:1234
+  };
+     
     
-      const [user, setUser] = useState({ name: "", password: "" });
+      const [user, setUser] = useState({ name: "", password: "",companyNo:"" });
       const [error, setError] = useState("");
     
       const Login = (details) => {
         console.log(details);
         if (
           details.password == adminUser.password &&
-          details.name == adminUser.name
+          details.name == adminUser.name &&
+          details.companyNo== adminUser.companyNo
         ) {
           setError("")
           console.log("giriş başarılı");
           setUser({
             name: details.name,
             password: details.password,
+            companyNo:details.companyNo
           });
         } else {
           console.log("tekrar dene");
@@ -28,13 +32,15 @@ export const HomeContext=createContext();
         }
       };
       const Logout =()=>{
-        setUser({name: "", password: ""})
+        setUser({name: "", password: "",companyNo:""})
       }
       const contextValue={
         user:user,
         error:error,
         Login:Login,
-        Logout:Logout
+        Logout:Logout,
+        adminUser:adminUser
+        
 
       }
 
